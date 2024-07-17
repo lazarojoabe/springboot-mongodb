46343dcea6d64a6c809f357a2dc2c545940fd2fc
Lázaro Joabe <99762355+lazarojoabe@users.noreply.github.com>
2024-07-17 18:34:09 -0300

    first commit

---
+++ src/test/java/com/lazaro/springboot_mongodb/SpringbootMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lazaro.springboot_mongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
