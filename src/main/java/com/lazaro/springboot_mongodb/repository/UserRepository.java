047a0fc42f6376c7101edb5d6a86728c1cd75fb0
Lázaro Joabe <99762355+lazarojoabe@users.noreply.github.com>
2024-07-17 18:56:07 -0300

    Entity User and REST working

---
+++ src/main/java/com/lazaro/springboot_mongodb/repository/UserRepository.java
@@ -0,0 +1,9 @@
+//package com.lazaro.springboot_mongodb.repository;
+//
+//import org.springframework.data.mongodb.repository.MongoRepository;
+//import org.springframework.stereotype.Repository;
+//
+//@Repository
+//public interface UserRepository extends MongoRepository<T, ID>{
+//	
+//}
