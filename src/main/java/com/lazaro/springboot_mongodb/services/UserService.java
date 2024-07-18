85e57bfbdc0620f83fd4b1df0e67f243ebf4193b
Lázaro Joabe <99762355+lazarojoabe@users.noreply.github.com>
2024-07-18 17:05:34 -0300

    find user by id implemented

--- src/main/java/com/lazaro/springboot_mongodb/services/UserService.java
+++ src/main/java/com/lazaro/springboot_mongodb/services/UserService.java
@@ -1,20 +1,27 @@
 package com.lazaro.springboot_mongodb.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.lazaro.springboot_mongodb.domain.User;
 import com.lazaro.springboot_mongodb.repository.UserRepository;
+import com.lazaro.springboot_mongodb.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
 	@Autowired
 	private UserRepository repository;
 	
-	
+		
 	public List<User> findAll(){
 		return repository.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> user = repository.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException("Object not found!"));
 	}
 }
